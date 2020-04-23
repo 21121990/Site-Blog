2f73444608b45cfcb0dced2eb6d073c774b3f452
DAYVSON SILVINO DA SILVA <62804086+21121990@users.noreply.github.com>
2020-04-23 20:24:00 -0300

    Starting the project with creating category routes

---
+++ Blog/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+const categoriesController = require("./Categories/CategoriesController");
+const articlesController = require("./Articles/ArticlesController")
+
+//view engine
+app.set('view engine','ejs');
+
+//Static
+app.use(express.static('public'));
+
+//Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Conectar no banco
+connection.authenticate()
+.then(() =>{
+    console.log("Conexão com banco realizada");
+    }).catch((error) => {
+        console.log(error);
+    })
+
+app.use("/", categoriesController)
+app.use("/", articlesController)
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+
+app.listen(8080, ()=>{
+    console.log("Servidor online")
+});
