1504f26865797283e9fab084d16f9ef95df5722c
DAYVSON SILVINO DA SILVA <62804086+21121990@users.noreply.github.com>
2020-04-24 10:25:03 -0300

    Added route to delete categories

--- Blog/Categories/CategoriesController.js
+++ Blog/Categories/CategoriesController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const categories = require("./Categories")
+const Categories = require("./Categories")
 const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res) => {
@@ -9,19 +9,47 @@ router.get("/admin/categories/new", (req, res) => {
 
 router.post("/categories/save", (req, res) => {
     var title = req.body.title;
-    if(title != undefined){
+    if (title != undefined) {
 
-        categories.create({
+        Categories.create({
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect("/");
+            res.redirect("/admin/categories");
         })
 
-    }else{
+    } else {
         res.redirect("/admin/categories/new");
     }
-    
 });
+
+router.get("/admin/categories", (req, res) => {
+    Categories.findAll().then(categories => {
+        res.render("admin/categories/index", { categories: categories });
+    });
+});
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+    if (id != undefined) {
+        if (!isNaN(id)) {
+
+            Categories.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() =>{
+                res.redirect("/admin/categories");
+            });
+
+        } else { // Não for número
+            res.redirect("/admin/categories");
+        }
+    }else{ //null
+        res.redirect("/admin/categories");
+    }
+})
+
+
 
 module.exports = router;
