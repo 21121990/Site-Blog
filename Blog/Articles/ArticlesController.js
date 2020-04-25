e49fd8e2369d02e761bedf8559ebd4cd32136065
DAYVSON SILVINO DA SILVA <62804086+21121990@users.noreply.github.com>
2020-04-25 18:50:28 -0300

    Added page for articles and plugin for textarea

--- Blog/Articles/ArticlesController.js
+++ Blog/Articles/ArticlesController.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const Articles = require("./Articles");
+const Category = require("../Categories/Categories")
+const slugify = require("slugify");
 
-router.get("/Articles",(req, res)=>{
-    res.send("Rota de artigos")
+router.get("/admin/articles", (req, res) => {
+    Articles.findAll({
+        include:[{model: Category}]
+    }).then(articles =>{
+        res.render("admin/articles/index",{articles:articles})
+    });
+});
+
+router.get("/admin/articles/new", (req, res) => {
+    Category.findAll().then(categories =>{
+        res.render("admin/articles/new",{categories:categories})
+    })
+});
+
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+    
+    Articles.create({
+        title: title,
+        slug: slugify(title),
+        body:body,
+        categoryId: category
+    }).then(()=>{
+        res.redirect("/admin/articles");
+    });
+
 });
 
 module.exports = router;
